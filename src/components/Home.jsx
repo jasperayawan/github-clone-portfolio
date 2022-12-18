90e2b228fd281d1c0fdc44ae9f28f5f567a0fc8f
jasperayawan <ejayawan22@gmail.com>
2022-12-18 12:38:12 +0800

    Added and Updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import Profile from '../assets/jasp (2).png'
 import blueBadge from '../assets/My project (1).png'
 import Typewriter from 'typewriter-effect'
-import { FaFacebook, FaFolder, FaProjectDiagram,FaPhone, FaVideo, FaInternetExplorer, FaBars} from 'react-icons/fa'
+import { FaFacebook, FaFolder, FaProjectDiagram,FaPhone, FaVideo, FaInternetExplorer, FaBars, FaEye} from 'react-icons/fa'
 import achievement1 from '../assets/certifications/certificate1.png'
 import achievement2 from '../assets/certifications/Digital certicate.jpg'
 import achievement3 from '../assets/certifications/freecodecamp.png'
 import achievement4 from '../assets/certifications/python certificate.png'
 import { NavLink } from 'react-router-dom';
 import Logo from '../assets/logo-letter-j-glitch-distortion-vector-22855050-removebg-preview.png'
+import HTML from '../assets/html-removebg-preview.png'
+import CSS from '../assets/css-removebg-preview.png'
+import Js from '../assets/javascript.png'
+import Git from '../assets/pngegg.png'
+import Java from '../assets/pngegg (1).png'
+import Cplusplus from '../assets/pngegg (2).png';
+import Python from '../assets/pngegg (3).png';
+import Rreact from '../assets/pngegg (4).png';
+import Tailwind from '../assets/Tailwind-CSS-removebg-preview.png' 
+import MongOdb from '../assets/pngegg (5).png';
+import NoDE from '../assets/pngegg (6).png'
 
 
 
@@ -125,6 +136,9 @@ const Home = () => {
                     <div className='text-[#C9D1D9] border-b-[1px] border-gray-700 pb-4 mb-4'>
                         <p className='font-mono text-[12px]'>JasperAyawan/READ.md</p>
                         <h1 className='text-3xl mt-2 font-bold'>⛷ Meta Crafter</h1>
+                        <div className='mt-4'>
+                            <p className='font-mono text-[12px] bg-gray-600 rounded-lg px-[7px] w-[160px] justify-center items-center flex'>Digital Craftman /Frond-end Developer</p>
+                        </div>
                     </div>
 
                     <p className='text-[#C9D1D9] text-[14px]'>I'm full-stack developer and content creator building my version of the digital
@@ -137,7 +151,7 @@ const Home = () => {
                     <div className='mt-4 border-b-4 pb-8 mb-8 border-[#333]'>
                         <ul className='flex gap-[.2rem] flex-wrap'>
                             <li><a href="/" className='text-white bg-red-500 px-8 text-[13px] font-mono py-[0.3rem]'>SUBSCRIBE</a></li>
-                            <li><a href="/" className='text-white bg-yellow-500 px-8 text-[13px] font-mono py-[0.3rem]'>VIEWS</a></li>
+                            <li><a href="/" className='text-white bg-yellow-500 px-4 text-[13px] font-mono py-[0.3rem] flex justify-center items-center gap-2'><FaEye/> VIEWS</a></li>
                             <li><a href="/" className='text-white bg-blue-500 px-8 text-[13px] font-mono py-[0.3rem]'>FOLLOW</a></li>
                             <li><a href="/" className='text-white bg-green-500 px-8 text-[13px] font-mono py-[0.3rem]'>STARS</a></li>
                         </ul>
@@ -145,6 +159,41 @@ const Home = () => {
 
                     <div>
                         <h1 className='font-bold text-gray-400'>🧰 Languages and Tools</h1>
+                        <div className='flex justify-start items-center gap-2 py-4 flex-wrap'>
+                        <div>
+                            <img src={HTML} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={CSS} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={Js} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={Git} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={Java} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={Cplusplus} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={Python} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={Rreact} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={Tailwind} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={MongOdb} alt="" className='w-[30px]'/>
+                        </div>
+                        <div>
+                            <img src={NoDE} alt="" className='w-[30px]'/>
+                        </div>
+                        </div>
                     </div>
                 </div>
                 </div>
